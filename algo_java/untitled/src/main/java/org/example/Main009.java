1092ef92749af0a4e6723757653d1a88f375d783
NyeongC <cjfsud23@naver.com>
2024-09-12 23:08:17 +0900

    백준 - 소트인사이드(정렬)

---
+++ algo_java/untitled/src/main/java/org/example/Main009.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+//소트인사이드
+public class Main009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long num = Integer.parseInt(br.readLine());
+        ArrayList<Long> list = new ArrayList<>();
+        while(num>0){
+            list.add(num % 10);
+            num = num / 10;
+        }
+        Collections.sort(list,(a,b)->{
+            if(a<b)
+                return 0;
+            else
+                return -1;
+        });
+        for(Long i : list)
+            System.out.print(i);
+    }
+}
