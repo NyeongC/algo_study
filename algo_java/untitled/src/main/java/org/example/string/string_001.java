d1981e050f47cca4f99bf79eec0b8b707a66a9ab
NyeongC <cjfsud23@naver.com>
2025-01-01 18:22:51 +0900

    프그 크기가 작은 부분문자열 / string 문제, 순간 substring이 생각안나서 빌더 append 함

---
+++ algo_java/untitled/src/main/java/org/example/string/string_001.java
@@ -0,0 +1,28 @@
+package org.example.string;
+
+public class string_001 {
+/*
+* 크기가 작은 부분문자열
+* */
+    public int solution(String t, String p) {
+        int answer = 0;
+
+        String [] temp = t.split("");
+        int l = p.length();
+
+
+        for(int i=0; i<=t.length()-l; i++) {
+            StringBuilder sb = new StringBuilder();
+            for(int j=i; j<i+l; j++){
+                sb.append(temp[j]);
+            }
+            if(Long.parseLong(sb.toString())<=Long.parseLong(p))
+                answer++;
+
+        }
+
+
+
+        return answer;
+    }
+}
