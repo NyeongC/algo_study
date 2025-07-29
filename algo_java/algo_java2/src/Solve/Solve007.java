de390faab373eb1dc6616af6cbe837e1719c6314
NyeongC <cjfsud23@naver.com>
2025-07-29 23:09:05 +0900

    프로그래머스 뒤에 있는 큰 수 찾기 스택

---
+++ algo_java/algo_java2/src/Solve/Solve007.java
@@ -0,0 +1,33 @@
+package Solve;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/*
+프로그래머스 뒤에 있는 큰 수 찾기
+스택
+*/
+public class Solve007 {
+
+    public int[] solution(int[] numbers) {
+        int len = numbers.length;
+        int[] answer = new int[len];
+        Stack<Integer> st = new Stack<>();
+
+        st.add(0);
+        for (int i = 0; i < len; i++) {
+            while (!st.isEmpty() && numbers[st.peek()] < numbers[i]) {
+                answer[st.pop()] = numbers[i];
+            }
+            st.add(i);
+        }
+
+        while(!st.isEmpty()) {
+            answer[st.pop()] = -1;
+        }
+
+        return answer;
+    }
+
+}
