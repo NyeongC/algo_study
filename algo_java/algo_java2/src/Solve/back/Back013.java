7495230f5b2a6eea9a31c478fe50d8bf09c5ce54
NyeongC <cjfsud23@naver.com>
2025-08-01 16:07:29 +0900

    백준 로봇 청소기 / 구현 문제 ※주의사항 구현 문제는 정확한 구현 필요 현재 방향을 유지한채 후진이 가능한지 -> 기존 방향은 그대로 냅두고 새로운 변수를 써야 함

--- algo_java/algo_java2/src/Solve/back/Back013.java
+++ algo_java/algo_java2/src/Solve/back/Back013.java
@@ -4,28 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+3 3
+1 1 0
+1 1 1
+1 0 1
+1 1 1
+ */
 public class Back013 {
 
-    // 북 동 남 서
-    static int[][] dir = {{-1, 0},{0, 1},{1, 0},{0 ,-1}};
     static int n;
     static int m;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
     static int[][] maps;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
+
         n = Integer.parseInt(input[0]);
         m = Integer.parseInt(input[1]);
-
         maps = new int[n][m];
-
+        int answer = 0;
         input = br.readLine().split(" ");
+
         int x = Integer.parseInt(input[0]);
         int y = Integer.parseInt(input[1]);
         int d = Integer.parseInt(input[2]);
-        int answer = 0;
 
         for (int i = 0; i < n; i++) {
             input = br.readLine().split(" ");
@@ -33,56 +40,78 @@ public class Back013 {
                 maps[i][j] = Integer.parseInt(input[j]);
             }
         }
-
+        int count = 0;
         while (true) {
-            // 현재 칸 청소
+            count++;
+            // 현재칸이 청소되지 않았다면 청소
             if (maps[x][y] == 0) {
                 maps[x][y] = 2;
                 answer++;
             }
 
-            if (!check(x, y)) {
-                // 후진
-                int backDir = (d + 2) % 4;
-                int nx = x + dir[backDir][0];
-                int ny = y + dir[backDir][1];
+//            System.out.println(x +" " + y);
+//            if(count > 100)
+//                break;
 
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m || maps[nx][ny] == 1) {
-                    break; // 후진 불가 시 종료
-                }
+            // 주변에 갈수있는곳이 없을때
+            if (!check(x, y)) {
+
+                // 후진
+                int back = (d + 2) % 4;
+                int nx = x + dx[back];
+                int ny = y + dy[back];
+
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+                    break;
+
+                if (maps[nx][ny] == 1)
+                    break;
 
                 x = nx;
                 y = ny;
                 continue;
             }
-
-            // 반시계 회전 후 한 칸 전진 시도
+            // 반시계 회전
             d = (d + 3) % 4;
-            int nx = x + dir[d][0];
-            int ny = y + dir[d][1];
 
-            if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 0) {
-                x = nx;
-                y = ny;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (!(nx < 0 || ny < 0 || nx >= n || ny >= m)) {
+                if (maps[nx][ny] == 0) {
+                    x = nx;
+                    y = ny;
+                }
+
+
             }
-            // 못가면 방향만 바꾸고 다음 루프에서 다시 체크
+
+
         }
+
 
         System.out.println(answer);
+
+        br.close();
+
     }
 
-    // 주변 4칸에 청소 안된 공간이 있는지 확인
     static boolean check(int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dir[i][0];
-            int ny = y + dir[i][1];
+        boolean result = false;
 
-            if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                if (maps[nx][ny] == 0) {
-                    return true;
-                }
-            }
+        for (int i = 0; i < 4; i++) {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+                continue;
+
+            if (maps[nx][ny] == 0)
+                result = true;
+
+
         }
-        return false;
+
+        return result;
     }
 }
